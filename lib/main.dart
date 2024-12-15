e791acc2a6b96877ed6dac49c248e764d4c3d9bc
Aleksander Bielicki <aleksanderbielicki2003@gmail.com>
2024-12-15 18:24:40 +0100

    Changed router settings so that the bottom nav bar stays for all routes (used ShellRoute)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gym_app/screens/add_exercise_screen.dart';
-import 'package:gym_app/screens/main_screen.dart';
+import 'package:gym_app/screens/exercise_list_screen.dart';
 import 'package:provider/provider.dart';
 
 import 'data/app_database.dart';
@@ -32,17 +32,98 @@ class MyApp extends StatelessWidget {
   }
 }
 
-final _router = GoRouter(
-  routes: [
+// final _router = GoRouter(
+//   routes: [
+//     GoRoute(
+//       path: '/',
+//       builder: (context, state) => MainScreen(),
+//       routes: [
+//         GoRoute(
+//           path: '/exercise/add',
+//           builder: (context, state) => const AddExerciseScreen(),
+//         ),
+//       ],
+//     )
+//   ],
+// );
+final _router = GoRouter(initialLocation: '/home', routes: [
+  ShellRoute(builder: (context, state, child) => BottomNavBar(child), routes: [
     GoRoute(
-      path: '/',
-      builder: (context, state) => MainScreen(),
-      routes: [
-        GoRoute(
-          path: '/exercise/add',
-          builder: (context, state) => const AddExerciseScreen(),
-        ),
-      ],
+      path: '/home',
+      builder: (context, state) => const Placeholder(),
+    ),
+    GoRoute(
+        path: '/exercise',
+        builder: (context, state) => const ExerciseScreen(),
+        routes: [
+          GoRoute(
+            path: 'add',
+            builder: (context, state) => const AddExerciseScreen(),
+          )
+        ]),
+    GoRoute(
+      path: '/social',
+      builder: (context, state) => const Placeholder(),
+    ),
+    GoRoute(
+      path: '/profile',
+      builder: (context, state) => const Placeholder(),
     )
-  ],
-);
+  ])
+]);
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar(this.child, {super.key});
+  final Widget child;
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Social',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Train',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          switch (index) {
+            case 0:
+              context.go('/home');
+            case 1:
+              context.go('/social');
+            case 2:
+              context.go('/exercise');
+            case 3:
+              context.go('/profile');
+          }
+        },
+      ),
+      body: widget.child,
+    );
+  }
+}
